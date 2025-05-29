3968c5be3d28e39544ffb26de010b985c060ffcd
IrisawaKazan <241301@st.yoshida-g.ac.jp>
2025-05-29 12:06:52 +0900

    rrrttt

---
+++ CplusDX/2025.05.29/CplusDX/enemy.h
@@ -0,0 +1,60 @@
+//==============================================================
+//
+// [enemy.h]
+// Author: Irisawa Kazan
+//
+//==============================================================
+#ifndef _ENEMY_H_ // このマクロ定義がされてなかったら
+#define _ENEMY_H_ // 2重インクルード防止のマクロ定義
+
+#include"main.h"
+#include"object2D.h"
+
+// マクロ定義
+#define MAX_ENEMYTEX (5)
+#define ENEMY_SIZEX (50.0f)
+#define ENEMY_SIZEY (50.0f)
+
+// エネミークラス
+class CEnemy : public CObject2D
+{
+public:
+	typedef enum
+	{
+		ETYPE_NONE = 0,
+		ETYPE_000, // 赤い丸
+		ETYPE_001, // 黄色いひし形
+		ETYPE_002, // 青い五角形
+		ETYPE_003, // ベージュのトゲトゲ
+
+		ETYPE_MAX
+	}ETYPE;
+
+	CEnemy(int nPriority = 3);
+	~CEnemy();
+
+	static HRESULT Load(void);
+	static void Unload(void);
+
+	static CEnemy* Create(D3DXVECTOR3 pos, float xsize, float ysize, ETYPE type);
+
+	HRESULT Init(D3DXVECTOR3 pos);
+	void Uninit(void);
+	void Update(void);
+	void Draw(void);
+
+	void SetPosition(D3DXVECTOR3 pos);
+	void SetSize(float xsize, float ysize);
+
+	void SetType(ETYPE type);
+	ETYPE GetType(void);
+
+private:
+	static LPDIRECT3DTEXTURE9 m_pTexture[MAX_ENEMYTEX];
+	D3DXVECTOR3 m_move;
+	int m_nLife; // 寿命
+	ETYPE m_type;
+	int m_nCntTimer;
+};
+
+#endif
