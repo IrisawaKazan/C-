4c6ded27a93ba4b9f018fb69b4390b4362142649
IrisawaKazan <241301@st.yoshida-g.ac.jp>
2025-06-12 15:54:02 +0900

    1

---
+++ CplusDX/2025.06.12/CplusDX/renderer.h
@@ -0,0 +1,32 @@
+//==============================================================
+//
+// [renderer.h]
+// Author: Irisawa Kazan
+//
+//==============================================================
+#ifndef _RENDERER_H_ // このマクロ定義がされてなかったら
+#define _RENDERER_H_ // 2重インクルード防止のマクロ定義
+
+#include"main.h"
+
+// レンダラークラス
+class CRenderer
+{
+public:
+	CRenderer();
+	~CRenderer();
+
+	HRESULT Init(HWND hWnd, BOOL bWindow);
+	void Uninit(void);
+	void Update(void);
+	void Draw(void);
+
+	LPDIRECT3DDEVICE9 GetDevice(void);
+
+private:
+	LPDIRECT3D9 m_pD3D;
+	LPDIRECT3DDEVICE9 m_pD3DDevice;
+	static int m_nCntFPS;
+};
+
+#endif
