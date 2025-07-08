d4787cf728f68db5354a8f4e5d2a7598c088caa1
IrisawaKazan <241301@st.yoshida-g.ac.jp>
2025-07-08 11:29:31 +0900

    q

---
+++ CplusDX/2025.07.08/CplusDX/input.h
@@ -0,0 +1,123 @@
+//==============================================================
+//
+// [input.h]
+// Author: Irisawa Kazan
+//
+//==============================================================
+#ifndef _INPUT_H_ // このマクロ定義がされてなかったら
+#define _INPUT_H_ // 2重インクルード防止のマクロ定義
+
+#include"main.h"
+
+// マクロ定義
+#define NUM_KEY_MAX (256) // キーの最大数
+
+// 入力クラス
+class CInput
+{
+public:
+	CInput();
+	virtual ~CInput();
+
+	virtual HRESULT Init(HINSTANCE hInstance, HWND hWnd);
+	virtual void Uninit(void);
+	virtual void Update(void) = 0;
+
+protected:
+	static LPDIRECTINPUT8 m_pInput;
+	LPDIRECTINPUTDEVICE8 m_pDevice;
+
+private:
+
+};
+
+// キーボードクラス
+class CInputKeyboard : public CInput
+{
+public:
+	CInputKeyboard();
+	~CInputKeyboard();
+
+	HRESULT Init(HINSTANCE hInstance, HWND hWnd);
+	void Uninit(void);
+	void Update(void);
+
+	bool GetPress(int nKey);
+	bool GetTrigger(int nKey);
+
+private:
+	BYTE m_aKeyStatePress[NUM_KEY_MAX];
+	BYTE m_aKeyStateTrigger[NUM_KEY_MAX];
+};
+
+// マウスクラス
+class CInputMouse : public CInput
+{
+public:
+	// マウスの種類
+	typedef enum
+	{
+		MOUSE_LEFTBUTTON = 0, // 左クリック
+		MOUSE_RIGHTBUTTON,    // 右クリック
+		MOUSE_WHEELBUTTON,    // ホイール
+
+		MOUSE_MAX
+	}MOUSE;
+
+	CInputMouse();
+	~CInputMouse();
+
+	HRESULT Init(HINSTANCE hInstance, HWND hWnd);
+	void Uninit(void);
+	void Update(void);
+
+	bool Push(MOUSE type);
+	bool Release(MOUSE type);
+
+
+private:
+
+};
+
+// ジョイパッドクラス
+class CInputJoypad : public CInput
+{
+public:
+	typedef enum
+	{
+		JOYKEY_UP = 0,
+		JOYKEY_DOWN,
+		JOYKEY_LEFT,
+		JOYKEY_RIGHT,
+		JOYKEY_START,
+		JOYKEY_BACK,
+		JOYKEY_LS,
+		JOYKEY_RS,
+		JOYKEY_LEFT_B,
+		JOYKEY_RIGHT_B,
+		JOYKEY_LEFT_TRIGGER,
+		JOYKEY_RIGHT_TRIGGER,
+		JOYKEY_A,
+		JOYKEY_B,
+		JOYKEY_X,
+		JOYKEY_Y,
+
+		JOYKEY_MAX
+	}JOYKEY;
+
+	CInputJoypad();
+	~CInputJoypad();
+
+	HRESULT Init(HINSTANCE hInstance, HWND hWnd);
+	void Uninit(void);
+	void Update(void);
+
+	bool GetJoypadPress(JOYKEY key);
+	bool GetJoypadTrigger(JOYKEY key);
+
+private:
+	XINPUT_STATE m_joykeyState;
+	XINPUT_STATE m_joykeyOldState;
+};
+
+#endif
---
+++ CplusDX/2025.07.08/CplusDX/sound.h
@@ -0,0 +1,57 @@
+//==============================================================
+//
+// [sound.h]
+// Author: Irisawa Kazan
+//
+//==============================================================
+#ifndef _SOUND_H_ // このマクロ定義がされてなかったら
+#define _SOUND_H_ // 2重インクルード防止のマクロ定義
+
+#include"main.h"
+
+// サウンドクラス
+class CSound
+{
+public:
+	// サウンド一覧
+	typedef enum
+	{
+		SOUND_LABEL_MAINBGM = 0, // メインのBGM
+		SOUND_LABEL_SAMPLESE,
+
+		SOUND_LABEL_MAX,
+	} SOUND_LABEL;
+
+	CSound();
+	~CSound();
+
+	HRESULT Init(HWND hWnd);
+	void Uninit(void);
+	HRESULT PlaySound(SOUND_LABEL label);
+	void Stop(SOUND_LABEL label);
+	void Stop(void);
+	HRESULT CheckChunk(HANDLE hFile, DWORD format, DWORD* pChunkSize, DWORD* pChunkDataPosition);
+	HRESULT ReadChunkData(HANDLE hFile, void* pBuffer, DWORD dwBuffersize, DWORD dwBufferoffset);
+
+private:
+	typedef struct
+	{
+		const char* m_pFilename; // ファイル名
+		int m_nCntLoop;          // ループカウント
+	} SOUNDINFO;
+
+	IXAudio2* m_pXAudio2;                                // XAudio2オブジェクトへのインターフェイス
+	IXAudio2MasteringVoice* m_pMasteringVoice;           // マスターボイス
+	IXAudio2SourceVoice* m_apSourceVoice[SOUND_LABEL_MAX]; // ソースボイス
+	BYTE* m_apDataAudio[SOUND_LABEL_MAX];                  // オーディオデータ
+	DWORD m_aSizeAudio[SOUND_LABEL_MAX];                   // オーディオデータサイズ
+	SOUNDINFO m_aSoundInfo[SOUND_LABEL_MAX] = 
+	{
+		//{"data/BGM/sample_bgm.wav", -1}, // サンプルメインBGM
+		{"data/BGM/titleBGM.wav", -1},   // タイトルBGM
+		{"data/SE/sample_se.wav", 0},    // サンプルSE
+
+	};
+};
+
+#endif
