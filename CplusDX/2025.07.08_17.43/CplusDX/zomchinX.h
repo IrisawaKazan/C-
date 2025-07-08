da1cb8ae00e4c229dfe6b7fb678075bdc9a39985
IrisawaKazan <241301@st.yoshida-g.ac.jp>
2025-07-08 17:44:36 +0900

    u

---
+++ CplusDX/2025.07.08_17.43/CplusDX/zomchinX.h
@@ -0,0 +1,67 @@
+//==============================================================
+//
+// [zomchinX.h]
+// Author: Irisawa Kazan
+//
+//==============================================================
+#ifndef _ZOMCHINX_H_ // このマクロ定義がされてなかったら
+#define _ZOMCHINX_H_ // 2重インクルード防止のマクロ定義
+
+#include"main.h"
+#include"object.h"
+#include"manager.h"
+
+// マクロ定義
+#define ZOMCHIN_UNINIT_POS (ZOMCHIN_000_POS_X + 50.0f)
+
+// エネミーXクラス
+class CZomchinX : public CObject
+{
+public:
+	// ZomchinXの種類
+	typedef enum
+	{
+		ZOMCHIN_NONE = 0,
+		ZOMCHIN_000_A, // 棒→
+		ZOMCHIN_000_B, // ←棒
+
+		ZOMCHIN_MAX
+	}ZOMCHIN;
+
+	CZomchinX(int nPriority = 3);
+	~CZomchinX();
+
+	static CZomchinX* Create(D3DXVECTOR3 pos, ZOMCHIN type);
+
+	HRESULT Init(D3DXVECTOR3 pos);
+	void Uninit(void);
+	void Update(void);
+	void Draw(void);
+
+	void SetPosition(D3DXVECTOR3 pos);
+	void SetRotation(D3DXVECTOR3 rot);
+
+	D3DXVECTOR3 GetPos(void);
+	D3DXVECTOR3 GetRot(void);
+
+	void SetType(ZOMCHIN type);
+	ZOMCHIN GetType(void);
+
+	void Collision(void);
+
+private:
+	LPDIRECT3DTEXTURE9 m_pTexture;
+	LPD3DXMESH m_pMesh;      // メッシュ(頂点座標)へのポインタ
+	LPD3DXBUFFER m_pBuffMat; // マテリアルへのポインタ
+	DWORD m_dwNumMat;        // マテリアルの数
+	D3DXVECTOR3 m_pos;       // 位置
+	D3DXVECTOR3 m_rot;       // 向き
+	D3DXMATRIX m_mtxWorld;   // ワールドマトリックス
+
+	float m_fRotation; // ローテーション
+	ZOMCHIN m_type;
+	D3DXVECTOR3 m_vtxMin; // モデルの最小値
+	D3DXVECTOR3 m_vtxMax; // モデルの最大値
+};
+
+#endif
